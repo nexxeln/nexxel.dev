8658e6ec3bd95d99971af5b01fa5720e3536d6cc
Shoubhit Dash <shoubhit2005@gmail.com>
2022-07-17 14:30:43 +0530

    use path alias

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,6 +1,5 @@
 // src/server/db/client.ts
 import { PrismaClient } from "@prisma/client";
-import { env } from "../env";
 
 declare global {
   var prisma: PrismaClient | undefined;
@@ -12,6 +11,6 @@ export const prisma =
     log: ["query"],
   });
 
-if (env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== "production") {
   global.prisma = prisma;
 }
--- src/server/router/context.ts
+++ src/server/router/context.ts
@@ -1,7 +1,7 @@
 // src/server/router/context.ts
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
-import { prisma } from "../db/client";
+import { prisma } from "~/server/db/client";
 
 export const createContext = (opts?: trpcNext.CreateNextContextOptions) => {
   const req = opts?.req;
