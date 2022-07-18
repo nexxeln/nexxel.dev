247db7c600fb31885a360f403b96ba22f331e0eb
Shoubhit Dash <shoubhit2005@gmail.com>
2022-07-18 15:05:23 +0530

    plan out stuff

---
+++ src/pages/guestbook.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Guestbook = () => {
+  return <div>Guestbook</div>;
+};
+
+export default Guestbook;
+
+// supabase? or a postgres on railway + next-auth
---
+++ src/pages/stats.tsx
@@ -0,0 +1,7 @@
+const Stats = () => {
+  return <div>Stats</div>;
+};
+
+export default Stats;
+
+// gh followers, spotify, blog views?, number animations
