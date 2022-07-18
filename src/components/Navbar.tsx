b3b3b7606992ba98cfd3ea9eecbb7247f706c5ec
Shoubhit Dash <shoubhit2005@gmail.com>
2022-07-18 10:55:43 +0530

    basic nav item component done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { NextRouter } from "next/router";
 import { FC } from "react";
+import { clsx } from "clsx";
 
 const NavItem: FC<{ href: string; text: string; router: NextRouter }> = ({
   href,
@@ -9,5 +10,17 @@ const NavItem: FC<{ href: string; text: string; router: NextRouter }> = ({
 }) => {
   const isActive = router.pathname === (href === "/home" ? "/" : href);
 
-  return <Link href={href === "/home" ? "/" : href}></Link>;
+  return (
+    <Link href={href === "/home" ? "/" : href}>
+      <a
+        className={clsx(
+          "text-lg mr-4",
+          isActive &&
+            "text-transparent bg-clip-text bg-gradient-to-r from-t-pink via-t-purple to-t-orange"
+        )}
+      >
+        {text}
+      </a>
+    </Link>
+  );
 };
