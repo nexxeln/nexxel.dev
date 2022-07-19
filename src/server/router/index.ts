c1a242cd465895f8611f50e1fce7fb536b2eeabd
Shoubhit Dash <shoubhit2005@gmail.com>
2022-07-19 20:08:27 +0530

    Remove example

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,11 +2,7 @@
 import { createRouter } from "./context";
 import superjson from "superjson";
 
-import { exampleRouter } from "./example";
-
-export const appRouter = createRouter()
-  .transformer(superjson)
-  .merge("example.", exampleRouter);
+export const appRouter = createRouter().transformer(superjson);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
