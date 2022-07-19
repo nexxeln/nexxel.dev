598280552c894dca8f3893bae73d3c6139b7ed88
Shoubhit Dash <shoubhit2005@gmail.com>
2022-07-20 00:10:35 +0530

    nice styles

--- src/components/guestbook/Input.tsx
+++ src/components/guestbook/Input.tsx
@@ -1,6 +1,20 @@
+import clsx from "clsx";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import { useState } from "react";
+
+const LogOutButton = () => {
+  return (
+    <button
+      className="px-3 py-2 mt-2 text-sm transition-colors duration-300 border-2 rounded-md cursor-pointer border-opacity-80 border-t-purple hover:bg-t-purple hover:bg-opacity-30 hover:text-white"
+      onClick={() => {
+        signOut();
+      }}
+    >
+      Log Out
+    </button>
+  );
+};
 
 const Input = () => {
   const { data: session, status } = useSession();
@@ -9,8 +23,7 @@ const Input = () => {
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
+  const handleSubmit = () => {
     setLoading(true);
 
     if (message.length === 0) {
@@ -48,34 +61,40 @@ const Input = () => {
         )}
 
         <div className="pt-3" />
-        <form onSubmit={handleSubmit}>
-          <p className="text-sm text-t-red">{error}</p>
-          <input
-            type="text"
-            name="message"
-            id="message"
-            placeholder="Your message..."
-            maxLength={100}
-            className="w-full px-4 py-2 mt-1 text-xl border-2 rounded-md bg-zinc-800 focus:outline-none focus:border-opacity-100 border-opacity-80 border-t-pink text-slate-200"
-            onChange={(e) => setMessage(e.target.value)}
-          />
 
+        <p className="text-sm text-t-red">{error}</p>
+        <input
+          type="text"
+          name="message"
+          id="message"
+          placeholder="Your message..."
+          className="w-full px-4 py-2 mt-1 text-xl border-2 rounded-md bg-zinc-800 focus:outline-none focus:border-opacity-100 border-opacity-80 border-t-pink text-slate-200"
+          onChange={(e) => setMessage(e.target.value)}
+        />
+
+        <div className="flex items-center justify-between gap-2">
           <div className="flex gap-2">
-            <input
+            <button
               type="submit"
-              value="Sign"
               disabled={loading}
               className="px-3 py-2 mt-2 text-sm transition-colors duration-300 border-2 rounded-md cursor-pointer border-opacity-80 border-t-purple hover:bg-t-purple hover:bg-opacity-30 hover:text-white disabled:opacity-80"
-            />
-
-            <button
-              className="px-3 py-2 mt-2 text-sm transition-colors duration-300 border-2 rounded-md cursor-pointer border-opacity-80 border-t-purple hover:bg-t-purple hover:bg-opacity-30 hover:text-white"
-              onClick={() => signOut()}
+              onClick={() => handleSubmit()}
             >
-              Log Out
+              Sign
             </button>
+
+            <LogOutButton />
           </div>
-        </form>
+
+          <p
+            className={clsx(
+              "text-lg",
+              message.length > 100 ? "text-t-red" : "text-t-pink"
+            )}
+          >
+            {message.length}/100
+          </p>
+        </div>
       </>
     );
   }
