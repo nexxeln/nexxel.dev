c6352013baef1f589198b099af202aabf20c3997
Shoubhit Dash <shoubhit2005@gmail.com>
2022-07-24 16:10:06 +0530

    add query for gh stars

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,2 +1,37 @@
-const Stats = () => {};
+import clsx from "clsx";
+import { FC } from "react";
+import { trpc } from "~/utils/trpc";
+
+const StatsCard: FC<{ text: string; value: string }> = ({ text, value }) => {
+  return (
+    <div className="flex flex-col items-center border-2 rounded-md border-t-orange">
+      <h2 className={clsx(value === "..." ? "animate-pulse" : "")}>{value}</h2>
+      <h3>{text}</h3>
+    </div>
+  );
+};
+
+const Stats = () => {
+  const { data: followers, isLoading: followersIsLoading } = trpc.useQuery([
+    "stats.getGithubFollowers",
+  ]);
+
+  const { data: stars, isLoading: starsIsLoading } = trpc.useQuery([
+    "stats.getGithubStars",
+  ]);
+
+  return (
+    <div className="grid grid-cols-2 gap-4">
+      <StatsCard
+        text="GitHub Followers"
+        value={followersIsLoading ? "..." : followers?.followers}
+      />
+      <StatsCard
+        text="GitHub Stars"
+        value={starsIsLoading ? "..." : stars?.stars}
+      />
+    </div>
+  );
+};
+
 export default Stats;
