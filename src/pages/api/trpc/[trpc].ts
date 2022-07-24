decc0c65169304d7f701d7d1d14816f63a8bfef9
Shoubhit Dash <shoubhit2005@gmail.com>
2022-07-24 13:43:11 +0530

    add query for gh followers and cache response

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -7,4 +7,24 @@ import { createContext } from "~/server/router/context";
 export default createNextApiHandler({
   router: appRouter,
   createContext: createContext,
+  responseMeta({ ctx, paths, type, errors }) {
+    // assuming you have all your public routes with the keyword `public` in them
+    const allPublic = paths && paths.every((path) => path.includes("stats"));
+    // checking that no procedures errored
+    const allOk = errors.length === 0;
+    // checking we're doing a query request
+    const isQuery = type === "query";
+
+    if (ctx?.res && allPublic && allOk && isQuery) {
+      // cache request for 1 day + revalidate once every second
+      const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
+      return {
+        headers: {
+          "cache-control": `s-maxage=1, stale-while-revalidate=${ONE_DAY_IN_SECONDS}`,
+        },
+      };
+    }
+
+    return {};
+  },
 });
--- src/server/router/subrouters/stats.ts
+++ src/server/router/subrouters/stats.ts
@@ -1,7 +1,11 @@
-import { resolve } from "path";
-import { z } from "zod";
 import { createRouter } from "../context";
 
-export const statsRouter = createRouter().mutation("getGithubFollowers", {
-  async resolve() {},
+export const statsRouter = createRouter().query("getGithubFollowers", {
+  async resolve() {
+    const userResponse = await fetch("https://api.github.com/users/nexxeln");
+    const user = await userResponse.json();
+    return {
+      followers: user.followers,
+    };
+  },
 });
