3cfcce03839aa20cc640be2caefd1405246c0127
Shoubhit Dash <shoubhit2005@gmail.com>
2022-07-25 15:33:43 +0530

    create shortener page

---
+++ src/pages/shortener.tsx
@@ -0,0 +1,14 @@
+const ShortenerPage = () => {
+  return (
+    <div className="items-start">
+      <h1 className="text-3xl font-bold bold-text pt-14 text-t-purple">
+        Shortener
+      </h1>
+      <p className="pt-1 text-slate-200">
+        You can now use my domain to shorten your links!
+      </p>
+    </div>
+  );
+};
+
+export default ShortenerPage;
--- src/pages/shortner.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const shortner = () => {
-  return <div>shortner</div>;
-};
-
-export default shortner;
