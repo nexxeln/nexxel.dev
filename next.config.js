4f6bd373c1f55050e01f9169f9859cd1af9ccd32
Shoubhit Dash <shoubhit2005@gmail.com>
2022-08-09 12:37:51 +0530

    add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 const { withContentlayer } = require("next-contentlayer");
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -9,4 +12,4 @@ const nextConfig = {
   experimental: { images: { allowFutureImage: true } },
 };
 
-module.exports = withContentlayer(nextConfig);
+module.exports = withBundleAnalyzer(withContentlayer(nextConfig));
