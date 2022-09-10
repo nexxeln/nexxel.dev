02f91efd204666d1751d2fc8a49b2ba480c972eb
Shoubhit Dash <shoubhit2005@gmail.com>
2022-09-11 01:28:46 +0530

    add about page
    need to add images

---
+++ src/pages/about.tsx
@@ -0,0 +1,136 @@
+import Wrapper from "~/components/Wrapper";
+
+const linkStyles = "font-bold bold-text text-t-pink hover:opacity-90 transition-opacity duration-300"
+
+const AboutPage = () => {
+  return (
+    <Wrapper title="nexxel • about" description="Learn more about me.">
+      <h1 className="text-3xl font-bold bold-text pt-14 text-t-purple">
+        About Me
+      </h1>
+
+      <div className="pt-6" />
+
+      <div className="text-lg text-slate-200">
+
+        <p>Hi, I&apos;m <span className="font-bold bold-text">Shoubhit</span>, commonly known as <a href="https://why.nexxel.dev/" rel="noreferrer" target="_blank" className={linkStyles}>nexxel</a> online. I&apos;m
+          from and live&nbsp;(for now) in India. We got Computer Science
+          in our school in 5th grade, and since then it has been my
+          favourite subject at school, even though they taught us Java
+          which is not so enjoyable.
+          <br />
+          <br />
+          In <span className="font-bold bold-text">2021</span> I started learning <span className="font-bold bold-text">Python</span> from{" "}
+          <a
+            href="https://www.freecodecamp.org/"
+            rel="noreferrer"
+            target="_blank"
+            className={linkStyles}
+          >
+            freeCodeCamp
+          </a>{" "}
+          in my free time because I felt like I was just wasting a lot of
+          time, so I wanted to learn stuff that I will actually use in my
+          life. I made some <span className="font-bold bold-text">Discord bots</span>, wrote some <span className="font-bold bold-text">cool scripts</span>.
+          Eventually I got into <span className="font-bold bold-text">web development</span> and I love it, the
+          frontend, the backend, all of it. Lately I&apos;ve been dwelling
+          into some <span className="font-bold bold-text">Rust</span> and <span className="font-bold bold-text">Go</span> as well because I think thats the future.
+          I haven&apos;t gotten into it a lot because frankly I have no
+          time.
+          <br />
+          <br />
+          I&apos;m currently in <span></span> and preparing for{" "}
+          <a
+            href="https://en.wikipedia.org/wiki/Joint_Entrance_Examination"
+            rel="noreferrer"
+            target="_blank"
+            className={linkStyles}
+          >
+            JEE
+          </a>{" "}
+          which is an entrance exam for good engineering colleges in
+          India, so most of my time goes into studying for it. I&apos;m
+          hoping to study Computer Science in college.
+          <br />
+          <br />
+          For hobbies other than coding, I really like performing{" "}
+          <a
+            href="https://en.wikipedia.org/wiki/Magic_(illusion)"
+            rel="noreferrer"
+            target="_blank"
+            className={linkStyles}
+          >
+            magic
+          </a>
+          ,{" "}
+          <a
+            href="https://en.wikipedia.org/wiki/Mentalism"
+            rel="noreferrer"
+            target="_blank"
+            className={linkStyles}
+          >
+            mentalism
+          </a>
+          &nbsp;(yes I can read your mind) and I dabble into some{" "}
+          <a
+            href="https://www.youtube.com/watch?v=wx_dnEen2xQ"
+            rel="noreferrer"
+            target="_blank"
+            className={linkStyles}
+          >
+            cardistry
+          </a>{" "}
+          as well. I also like reading novels but I haven&apos;t read one
+          for a long time now, simply because I have no time. My favourite
+          author is{" "}
+          <a
+            href="https://en.wikipedia.org/wiki/John_Green"
+            rel="noreferrer"
+            target="_blank"
+            className={linkStyles}
+          >
+            John Green
+          </a>
+          . I also really like listening to music,{" "}
+          <a
+            href="https://open.spotify.com/artist/6bmlMHgSheBauioMgKv2tn?si=RoqNOQkQTaSoSmNIARTkHA"
+            rel="noreferrer"
+            target="_blank"
+            className={linkStyles}
+          >
+            Powfu
+          </a>
+          ,{" "}
+          <a
+            href="https://open.spotify.com/artist/1ScHz7wPPxVTEKsc9g3Z0c?si=uUlwcUFpSjq1HbxRGtea3Q"
+            rel="noreferrer"
+            target="_blank"
+            className={linkStyles}
+          >
+            Rxseboy
+          </a>
+          , and{" "}
+          <a
+            href="https://open.spotify.com/artist/1vwwjIPFeYoRfAUCqqO6cZ?si=VGnmIxdZQdCmtYiqVQGpZA"
+            rel="noreferrer"
+            target="_blank"
+            className={linkStyles}
+          >
+            44phantom
+          </a>{" "}
+          are some of my favourite artists. You can see more of my Spotify stats on{" "}
+          <a
+            href="https://s.nxl.sh/"
+            rel="noreferrer"
+            target="_blank"
+            className={linkStyles}
+          >
+            s.nxl.sh
+          </a>. I also like going on cycling trips with friends.
+        </p>
+      </div>
+    </Wrapper>
+  );
+}
+
+export default AboutPage;
