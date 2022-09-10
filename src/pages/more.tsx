d6615e36ae9319c83290fe756f56008f1701efc9
Shoubhit Dash <shoubhit2005@gmail.com>
2022-09-10 15:49:27 +0530

    add basic more page

---
+++ src/pages/more.tsx
@@ -0,0 +1,17 @@
+import Wrapper from "~/components/Wrapper";
+
+const MorePage = () => {
+  return (
+    <Wrapper
+      title="nexxel • more"
+      description="More things in my website"
+    >
+      <h1 className="text-3xl font-bold bold-text pt-14 text-t-purple">More</h1>
+      <p className="pt-1 text-slate-200">
+        This page was a result of me not wanting to overpopulate the navbar. Mostly random things which might interest you.
+      </p>
+    </Wrapper>
+  );
+}
+
+export default MorePage;
