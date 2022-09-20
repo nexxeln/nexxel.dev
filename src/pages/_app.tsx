03e4f9115b29df96836e00790f5e0a6f06cd7f83
Shoubhit Dash <shoubhit2005@gmail.com>
2022-09-20 13:47:41 +0530

    feat(app): upgrade to next 12.3.1 and use app type generic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,16 @@ import { withTRPC } from "@trpc/next";
 import { SessionProvider } from "next-auth/react";
 import superjson from "superjson";
 
+import type { AppType } from "next/app";
+import type { Session } from "next-auth";
 import type { AppRouter } from "~/server/router";
+
 import "~/styles/globals.css";
 
-const MyApp = ({ Component, pageProps: { session, ...pageProps } }: any) => {
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps }
+}) => {
   return (
     <>
       <Head>
