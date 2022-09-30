baee742e13a2cfd248db5e91e495d7cadaadcc84
Shoubhit Dash <shoubhit2005@gmail.com>
2022-09-30 19:09:17 +0530

    feat: remove no anim

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -39,7 +39,6 @@ const Blog = ({ posts }: { posts: Post[] }) => {
     <Wrapper
       title="nexxel • blog"
       description="Writings on programming and tech"
-      noAnim
     >
       <h1 className="bold-text pt-14 text-3xl font-bold text-t-purple">Blog</h1>
       <p className="pt-1 text-slate-200">
