1f5af47a916deecb12b2a9b9dba5537cdbfc4522
Shoubhit Dash <shoubhit2005@gmail.com>
2022-10-29 00:47:18 +0530

    feat: add hack font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,7 @@ module.exports = {
     extend: {
       fontFamily: {
         satoshi: ["Satoshi", ...defaultTheme.fontFamily.sans],
+        hack: ["Hack", ...defaultTheme.fontFamily.mono],
       },
     },
   },
