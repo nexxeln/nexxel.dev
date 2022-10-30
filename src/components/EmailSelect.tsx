ad1e9674091cb07e045de626c51a4375dc814073
Shoubhit Dash <shoubhit2005@gmail.com>
2022-10-30 15:24:41 +0530

    refactor: remove unused component

--- src/components/EmailSelect.tsx
+++ src/components/EmailSelect.tsx
@@ -1,33 +1,6 @@
 import { Menu, Transition } from "@headlessui/react";
 import { type ReactNode, Fragment } from "react";
 
-const classNames = (...classes: string[]) => {
-  return classes.filter(Boolean).join(" ");
-};
-
-type Props = {
-  href: string;
-  children: ReactNode;
-};
-
-const DropdownMenuItem = ({ href, children }: Props) => {
-  return (
-    <Menu.Item>
-      {({ active }) => (
-        <a
-          href={href}
-          className={classNames(
-            active ? "bg-orange-200 dark:bg-zinc-700" : "",
-            "block px-4 py-2 text-sm"
-          )}
-        >
-          {children}
-        </a>
-      )}
-    </Menu.Item>
-  );
-};
-
 export const EmailSelect = () => {
   return (
     <Menu as="div" className="relative inline-block text-left">
