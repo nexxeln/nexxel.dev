f57463e10ff376b5f9d122e84a0d66c7a5c7cc8b
Shoubhit Dash <shoubhit2005@gmail.com>
2022-10-31 11:40:50 +0530

    feat: add ids to headings

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import { remarkReadingTime } from "./src/utils/reading-time.mjs";
 
 export default defineConfig({
   site: "https://www.nexxel.dev",
   markdown: {
     extendDefaultPlugins: true,
+    rehypePlugins: [rehypeAutolinkHeadings],
     remarkPlugins: [remarkReadingTime],
     shikiConfig: {
       theme: "vitesse-dark",
