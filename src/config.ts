bb19be4f48fbf0d02c05108b7a52244905c53c47
Shoubhit Dash <shoubhit2005@gmail.com>
2022-10-31 12:35:33 +0530

    feat: seo go brr

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Shoubhit Dash";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "nexxel •";
+export const SITE_DESCRIPTION = "17 yo self-taught developer";
