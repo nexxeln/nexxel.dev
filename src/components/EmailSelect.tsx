8f736dad87b5123cf5d60708be0559919dfb7a48
Shoubhit Dash <shoubhit2005@gmail.com>
2022-11-30 22:39:58 +0530

    fix: font-medium in email select as well

--- src/components/EmailSelect.tsx
+++ src/components/EmailSelect.tsx
@@ -5,7 +5,7 @@ export const EmailSelect = () => {
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-        <Menu.Button className="text-t-green hover:underline underline-offset-[3px]">
+        <Menu.Button className="text-t-green hover:underline font-medium underline-offset-[3px]">
           Email
         </Menu.Button>
       </div>
