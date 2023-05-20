a87e51fb60de96898858f8ed6b99945b0ac473a8
Shoubhit Dash <shoubhit2005@gmail.com>
2023-05-20 23:54:06 +0530

    add link component

---
+++ src/components/Link.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const props = defineProps<{ href: string }>()
+</script>
+
+<template>
+  <a :href="props.href" class="underline decoration-neutral-300 underline-offset-4 transition-colors dark:decoration-neutral-700 focus:(decoration-neutral-400 outline-offset-6 dark:decoration-neutral-600) hover:(decoration-neutral-400 dark:decoration-neutral-600)">
+    <slot />
+  </a>
+</template>
