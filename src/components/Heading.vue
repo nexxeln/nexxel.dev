1586ed18ea50cff3b8dcd5503198105e2c86de56
Shoubhit Dash <shoubhit2005@gmail.com>
2023-05-20 23:31:32 +0530

    add heading component

---
+++ src/components/Heading.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+type HeadingLevel = "h1" | "h2" | "h3" | "h4" | "h5" | "h6"
+
+const props = defineProps<{
+  level: HeadingLevel
+}>()
+</script>
+
+<template>
+  <component :is="props.level" class="my-6 font-[555] [&:is(h1)]:text-2xl [&:is(h2)]:text-xl [&:is(h3)]:text-lg">
+    <slot />
+  </component>
+</template>
