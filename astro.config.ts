e77e73da8bc6d8d5c3cd96df73c87aaab42f83af
Shoubhit Dash <shoubhit2005@gmail.com>
2023-05-20 15:22:38 +0530

    astro add vue

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config"
 import unocss from "unocss/astro"
+import vue from "@astrojs/vue"
 
 export default defineConfig({
-  integrations: [unocss({ injectReset: true })],
+  integrations: [
+    unocss({ injectReset: true }),
+    vue(),
+  ],
 })
