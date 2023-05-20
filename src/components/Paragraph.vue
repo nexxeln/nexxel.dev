fa2d9aded2cd69de64695db9d8a4184709e55202
Shoubhit Dash <shoubhit2005@gmail.com>
2023-05-20 20:27:59 +0530

    add paragraph component

---
+++ src/components/Paragraph.vue
@@ -0,0 +1,5 @@
+<template>
+  <p class="m-2 max-w-prose overflow-x-hidden text-ellipsis p-2 line-height-7 [&:has(img)]:(mx-0 overflow-x-visible px-0) [&:has(a)]:(max-w-container whitespace-nowrap)">
+    <slot />
+  </p>
+</template>
