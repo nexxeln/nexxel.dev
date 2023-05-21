148d5eff1147c381ed40329d04814d51b7ae68c7
Shoubhit Dash <shoubhit2005@gmail.com>
2023-05-21 15:52:51 +0530

    placeholder font

--- uno.config.ts
+++ uno.config.ts
@@ -1,16 +1,25 @@
 import {
   defineConfig,
   presetUno,
+  presetWebFonts,
   transformerDirectives,
   transformerVariantGroup,
 } from "unocss"
 
 export default defineConfig({
-  presets: [presetUno()],
+  presets: [presetUno(), presetWebFonts({
+    provider: "fontshare",
+    fonts: {
+      sans: "Switzer",
+    },
+  })],
   transformers: [transformerDirectives(), transformerVariantGroup()],
   theme: {
     maxWidth: {
       container: "43rem",
     },
+  },
+  shortcuts: {
+    "text-imp": "text-neutral-900 dark:text-neutral-100",
   },
 })
