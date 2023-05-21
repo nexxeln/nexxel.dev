91bcceaf4903c55b91d01551b399c058130332a1
Shoubhit Dash <shoubhit2005@gmail.com>
2023-05-21 15:52:36 +0530

    external links open in new tab

--- src/components/Link.vue
+++ src/components/Link.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+import { computed } from "vue"
+
 const props = defineProps<{ href: string }>()
+
+const isExternal = computed(() => props.href.startsWith("http"))
 </script>
 
 <template>
-  <a :href="props.href" class="underline decoration-neutral-300 underline-offset-4 transition-colors dark:decoration-neutral-700 focus:(decoration-neutral-400 outline-offset-6 dark:decoration-neutral-600) hover:(decoration-neutral-400 dark:decoration-neutral-600)">
+  <a
+    :href="props.href"
+    :target="isExternal ? '_blank' : '_self'"
+    class="underline decoration-neutral-300 underline-offset-4 transition-colors dark:decoration-neutral-600 focus:(decoration-neutral-400 outline-offset-6 dark:decoration-neutral-500) hover:(decoration-neutral-400 dark:decoration-neutral-500)"
+  >
     <slot />
   </a>
 </template>
