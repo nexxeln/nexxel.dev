c21ba0d027aaf5f266a0fd3b8a37a70d29f8960d
Shoubhit Dash <shoubhit2005@gmail.com>
2023-05-22 10:18:49 +0530

    switzer font

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from "astro/config"
 import unocss from "unocss/astro"
 import vue from "@astrojs/vue"
+import { FontaineTransform } from "fontaine"
 
 export default defineConfig({
   integrations: [
     unocss({ injectReset: true }),
     vue(),
   ],
+  vite: {
+    plugins: [
+      FontaineTransform.vite({
+        fallbacks: ["BlinkMacSystemFont", "Segoe UI", "Helvetica Neue", "Arial", "Noto Sans"],
+        resolvePath: id => new URL(`./public${id}`, import.meta.url),
+      }),
+    ],
+  },
 })
--- uno.config.ts
+++ uno.config.ts
@@ -1,22 +1,19 @@
 import {
   defineConfig,
   presetUno,
-  presetWebFonts,
   transformerDirectives,
   transformerVariantGroup,
 } from "unocss"
 
 export default defineConfig({
-  presets: [presetUno(), presetWebFonts({
-    provider: "fontshare",
-    fonts: {
-      sans: "Switzer",
-    },
-  })],
+  presets: [presetUno()],
   transformers: [transformerDirectives(), transformerVariantGroup()],
   theme: {
     colors: {
       "neutral-850": "#1f1f1f",
+    },
+    fontFamily: {
+      sans: "Switzer",
     },
     maxWidth: {
       container: "43rem",
