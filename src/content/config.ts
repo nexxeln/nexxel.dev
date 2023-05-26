981dfcadc2fd464e28d40b93445eea96baa52646
Shoubhit Dash <shoubhit2005@gmail.com>
2023-05-27 00:09:50 +0530

    make draft optional

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,10 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-
 import { defineCollection } from "astro:content"
 import { z } from "zod"
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 const blog = defineCollection({
   type: "content",
   schema: z.object({
-    isDraft: z.boolean(),
+    isDraft: z.boolean().optional().default(false),
     title: z.string(),
     description: z.string(),
     date: z.string().transform(str => new Date(str)),
