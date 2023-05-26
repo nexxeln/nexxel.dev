368cacc70ff5aa9a51cfb295e463afaff7b50eb0
Shoubhit Dash <shoubhit2005@gmail.com>
2023-05-26 21:35:53 +0530

    astro add mdx

--- astro.config.ts
+++ astro.config.ts
@@ -2,18 +2,18 @@ import { defineConfig } from "astro/config"
 import unocss from "unocss/astro"
 import vue from "@astrojs/vue"
 import { FontaineTransform } from "fontaine"
+import mdx from "@astrojs/mdx"
 
 export default defineConfig({
   integrations: [
     unocss({ injectReset: true }),
     vue(),
+    mdx(),
   ],
   vite: {
-    plugins: [
-      FontaineTransform.vite({
-        fallbacks: ["BlinkMacSystemFont", "Segoe UI", "Helvetica Neue", "Arial", "Noto Sans"],
-        resolvePath: id => new URL(`./public${id}`, import.meta.url),
-      }),
-    ],
+    plugins: [FontaineTransform.vite({
+      fallbacks: ["BlinkMacSystemFont", "Segoe UI", "Helvetica Neue", "Arial", "Noto Sans"],
+      resolvePath: id => new URL(`./public${id}`, import.meta.url),
+    })],
   },
 })
