c4c7d92e0f79341da31abad99472922ac9fa88f9
Shoubhit Dash <shoubhit2005@gmail.com>
2023-05-27 10:14:50 +0530

    revert

--- astro.config.ts
+++ astro.config.ts
@@ -18,21 +18,7 @@ export default defineConfig({
     plugins: [
       FontaineTransform.vite({
         fallbacks: ["BlinkMacSystemFont", "Segoe UI", "Helvetica Neue", "Arial", "Noto Sans"],
-        // make sure resolve only Switzer
-        resolvePath: (id) => {
-          if (id !== "/fonts/Switzer.woff2")
-            return new URL("")
-          return new URL(`./public${id}`, import.meta.url)
-        },
-      }),
-      FontaineTransform.vite({
-        // monospace fallbacks
-        fallbacks: ["SFMono-Regular", "Menlo", "Monaco", "Consolas", "Liberation Mono", "Courier New", "monospace"],
-        resolvePath: (id) => {
-          if (id !== "/fonts/Hack.woff2")
-            return new URL("")
-          return new URL(`./public${id}`, import.meta.url)
-        },
+        resolvePath: id => new URL(`./public${id}`, import.meta.url),
       }),
     ],
   },
