c83fc1b894520d135d9caec3303e8f99e9ffe101
Shoubhit Dash <shoubhit2005@gmail.com>
2023-05-27 16:07:03 +0530

    sitemap and robots.txt

--- astro.config.ts
+++ astro.config.ts
@@ -2,13 +2,16 @@ import { defineConfig } from "astro/config"
 import vue from "@astrojs/vue"
 import mdx from "@astrojs/mdx"
 import solidJs from "@astrojs/solid-js"
+import sitemap from "@astrojs/sitemap"
 import unocss from "unocss/astro"
 import { FontaineTransform } from "fontaine"
 import vesper from "./src/lib/vesper.json"
 
 export default defineConfig({
+  site: "https://www.nexxel.dev",
   integrations: [
     mdx(),
+    sitemap(),
     solidJs(),
     unocss({ injectReset: true }),
     vue(),
