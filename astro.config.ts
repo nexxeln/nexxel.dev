be1d9b301bd987b4f9dd3d43375d7f3299918a08
Shoubhit Dash <shoubhit2005@gmail.com>
2023-05-27 09:56:36 +0530

    does this actually work

--- astro.config.ts
+++ astro.config.ts
@@ -15,9 +15,25 @@ export default defineConfig({
   // @ts-expect-error
   markdown: { shikiConfig: { theme: vesper, wrap: true } },
   vite: {
-    plugins: [FontaineTransform.vite({
-      fallbacks: ["BlinkMacSystemFont", "Segoe UI", "Helvetica Neue", "Arial", "Noto Sans"],
-      resolvePath: id => new URL(`./public${id}`, import.meta.url),
-    })],
+    plugins: [
+      FontaineTransform.vite({
+        fallbacks: ["BlinkMacSystemFont", "Segoe UI", "Helvetica Neue", "Arial", "Noto Sans"],
+        // make sure resolve only Switzer
+        resolvePath: (id) => {
+          if (id !== "/fonts/Switzer.woff2")
+            return new URL("")
+          return new URL(`./public${id}`, import.meta.url)
+        },
+      }),
+      FontaineTransform.vite({
+        // monospace fallbacks
+        fallbacks: ["SFMono-Regular", "Menlo", "Monaco", "Consolas", "Liberation Mono", "Courier New", "monospace"],
+        resolvePath: (id) => {
+          if (id !== "/fonts/Hack.woff2")
+            return new URL("")
+          return new URL(`./public${id}`, import.meta.url)
+        },
+      }),
+    ],
   },
 })
