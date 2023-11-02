bd7c26428e8e6cedd5a5b71b795fb0a57a9818ba
Shoubhit Dash <shoubhit2005@gmail.com>
2023-11-02 23:06:47 +0530

    add function to get top tracks from spotify

---
+++ src/lib/spotify.ts
@@ -0,0 +1,87 @@
+import { z } from "zod";
+
+export async function getAccessToken(): Promise<{ access_token: string }> {
+  const refreshToken = import.meta.env.SPOTIFY_REFRESH_TOKEN;
+  const clientId = import.meta.env.SPOTIFY_CLIENT_ID;
+  const clientSecret = import.meta.env.SPOTIFY_CLIENT_SECRET;
+
+  const response = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      Authorization: `Basic ${Buffer.from(
+        `${clientId}:${clientSecret}`
+      ).toString("base64")}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      grant_type: "refresh_token",
+      refresh_token: refreshToken,
+    }),
+  });
+
+  const { access_token } = z
+    .object({
+      access_token: z.string(),
+    })
+    .parse(await response.json());
+
+  return { access_token };
+}
+
+export async function getTopTracks() {
+  const { access_token } = await getAccessToken();
+
+  z.object({
+    artist: z.string(),
+    songUrl: z.string(),
+    title: z.string(),
+  });
+
+  // return fetch("https://api.spotify.com/v1/me/top/tracks?time_range=short_term", {
+  //   headers: {
+  //     "Authorization": `Bearer ${access_token}`
+  //   }
+  // })
+  const response = await fetch(
+    "https://api.spotify.com/v1/me/top/tracks?time_range=short_term",
+    {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    }
+  ).then((res) => res.json());
+
+  const { items } = z
+    .object({
+      items: z.array(
+        z.object({
+          artists: z.array(
+            z.object({
+              name: z.string(),
+            })
+          ),
+          album: z.object({
+            name: z.string(),
+            images: z.array(
+              z.object({
+                url: z.string(),
+              })
+            ),
+          }),
+          external_urls: z.object({
+            spotify: z.string(),
+          }),
+          name: z.string(),
+        })
+      ),
+    })
+    .parse(response);
+
+  return items.slice(0, 5).map((item) => ({
+    artists: item.artists,
+    songUrl: item.external_urls.spotify,
+    title: item.name,
+    album: item.album.name,
+    image: item.album.images[0].url,
+  }));
+}
