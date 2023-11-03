dd51effec6051cdbbac27e4f2f37c5df5f34ae2e
Shoubhit Dash <shoubhit2005@gmail.com>
2023-11-03 15:21:14 +0530

    add vercel adapter

--- astro.config.ts
+++ astro.config.ts
@@ -3,11 +3,14 @@ import vue from "@astrojs/vue"
 import mdx from "@astrojs/mdx"
 import solidJs from "@astrojs/solid-js"
 import sitemap from "@astrojs/sitemap"
+import vercel from '@astrojs/vercel/serverless';
 import unocss from "unocss/astro"
 import vesper from "./src/lib/vesper.json"
 
 export default defineConfig({
   site: "https://www.nexxel.dev",
+  adapter: vercel(),
+  output: "hybrid",
   integrations: [
     mdx(),
     sitemap(),
