0723b51aab5df99a16354c6cba6e5018579c2599
Shoubhit Dash <shoubhit2005@gmail.com>
2024-06-22 14:36:41 +0530

    rm redundant component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,5 @@
 import Link from "next/link";
 
-function RightArrowIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="h-5 w-5"
-    >
-      <path d="M7 7h10v10" />
-      <path d="M7 17 17 7" />
-    </svg>
-  );
-}
-
 function WorkIcon() {
   return (
     <svg
