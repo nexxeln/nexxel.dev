709e9d5cd67955dc88b1373fecbac4a159913b27
Shoubhit Dash <shoubhit2005@gmail.com>
2024-06-22 15:36:22 +0530

    we got posts showing up, thank you leerob

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import { getBlogPosts } from "~~/blog";
+
+export default function BlogPage() {
+  const posts = getBlogPosts().sort(
+    (a, b) =>
+      new Date(b.metadata.date).getTime() - new Date(a.metadata.date).getTime(),
+  );
+
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-medium tracking-tighter">blog</h1>
+
+      <div className="flex flex-col gap-8">
+        {posts.map((post) => (
+          <Link key={post.slug} href={`/blog/${post.slug}`} className="group">
+            <div className="flex w-full flex-col gap-y-1">
+              <p className="text-lg font-medium underline underline-offset-4">
+                {post.metadata.title.toLowerCase()}
+              </p>
+              <p className="prose prose-neutral dark:prose-invert">
+                {post.metadata.description.toLowerCase()}
+              </p>
+              <p className="text-sm text-neutral-600 dark:text-neutral-400">
+                {new Date(post.metadata.date)
+                  .toLocaleDateString("en-US", {
+                    month: "short",
+                    day: "numeric",
+                    year: "numeric",
+                  })
+                  .toLowerCase()}
+              </p>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </section>
+  );
+}
---
+++ src/blog.ts
@@ -0,0 +1,77 @@
+import fs from "fs";
+import path from "path";
+
+type Metadata = {
+  title: string;
+  description: string;
+  date: string;
+};
+
+type FrontmatterParseResult = {
+  metadata: Metadata;
+  content: string;
+};
+
+type MDXFileData = FrontmatterParseResult & {
+  slug: string;
+  tweetIds: string[];
+};
+
+function parseFrontmatter(fileContent: string): FrontmatterParseResult {
+  const frontmatterRegex = /---\s*([\s\S]*?)\s*---/;
+  const match = frontmatterRegex.exec(fileContent);
+  if (!match) {
+    throw new Error("Invalid frontmatter: No match found");
+  }
+  const frontMatterBlock = match[1];
+  if (!frontMatterBlock) {
+    throw new Error("Invalid frontmatter: No frontmatter block found");
+  }
+  const content = fileContent.replace(frontmatterRegex, "").trim();
+  const frontMatterLines = frontMatterBlock.trim().split("\n");
+  const metadata: Partial<Metadata> = {};
+
+  frontMatterLines.forEach((line) => {
+    const [key, ...valueArr] = line.split(": ");
+    let value = valueArr.join(": ").trim();
+    value = value.replace(/^['"](.*)['"]$/, "$1"); // Remove quotes
+    if (key && value) {
+      metadata[key.trim() as keyof Metadata] = value;
+    }
+  });
+
+  return { metadata: metadata as Metadata, content };
+}
+
+function getMDXFiles(dir: string): string[] {
+  return fs.readdirSync(dir).filter((file) => path.extname(file) === ".mdx");
+}
+
+function readMDXFile(filePath: string): FrontmatterParseResult {
+  const rawContent = fs.readFileSync(filePath, "utf-8");
+  return parseFrontmatter(rawContent);
+}
+
+function extractTweetIds(content: string): string[] {
+  const tweetMatches = content.match(/<StaticTweet\sid="[0-9]+"\s\/>/g);
+  return tweetMatches?.map((tweet) => tweet.match(/[0-9]+/)?.[0] ?? "") ?? [];
+}
+
+function getMDXData(dir: string): MDXFileData[] {
+  const mdxFiles = getMDXFiles(dir);
+  return mdxFiles.map((file) => {
+    const { metadata, content } = readMDXFile(path.join(dir, file));
+    const slug = path.basename(file, path.extname(file));
+    const tweetIds = extractTweetIds(content);
+    return {
+      metadata,
+      slug,
+      tweetIds,
+      content,
+    };
+  });
+}
+
+export function getBlogPosts(): MDXFileData[] {
+  return getMDXData(path.join(process.cwd(), "posts"));
+}
