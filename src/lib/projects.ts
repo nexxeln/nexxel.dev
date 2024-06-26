f269eefba1c115db2305583dbc14f5a8d286dc14
Shoubhit Dash <shoubhit2005@gmail.com>
2024-06-26 11:57:17 +0530

    lowercase

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -25,32 +25,32 @@ export async function getProjects(): Promise<Project[]> {
     {
       repo: "create-t3-app",
       link: "https://github.com/nexxeln/create-t3-app",
-      description: "The best way to start a full-stack, typesafe Next.js app",
+      description: "the best way to start a full-stack, typesafe next.js app",
     },
     {
       repo: "nexxel.dev",
       link: "https://github.com/nexxeln/nexxel.dev",
-      description: "Boring personal site built with Astro",
+      description: "boring personal site built with astro",
     },
     {
       repo: "license-generator",
       link: "https://github.com/nexxeln/license-generator",
-      description: "Create licenses for your projects right from your terminal",
+      description: "create licenses for your projects right from your terminal",
     },
     {
       repo: "spotify-voice-control",
       link: "https://github.com/nexxeln/spotify-voice-control",
-      description: "Voice control for Spotify through the terminal",
+      description: "voice control for spotify through the terminal",
     },
     {
       repo: "hackernews",
       link: "https://github.com/nexxeln/hackernews",
-      description: "Hacker News clone made with SolidStart and tRPC",
+      description: "hacker news clone made with solidstart and tRPC",
     },
     {
       repo: "dots",
       link: "https://github.com/nexxeln/dots",
-      description: "My dotfiles for MacOS",
+      description: "my dotfiles for macos",
     },
   ];
 }
