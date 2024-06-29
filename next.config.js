2fdccc9fe9ddb8555dd5098e7f9a72e50e96ca10
Shoubhit Dash <shoubhit2005@gmail.com>
2024-06-29 14:06:38 +0530

    ppr

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
   transpilePackages: ["next-mdx-remote"],
+  experimental: {
+    ppr: true,
+  },
 };
 
 export default config;
