5ef798dc3963dfed95fd6634431b51419dad22df
Shoubhit Dash <shoubhit2005@gmail.com>
2024-06-29 19:10:08 +0530

    send welcome email

--- src/app/blog/actions.ts
+++ src/app/blog/actions.ts
@@ -10,13 +10,13 @@ export const subscribe = async (formData: FormData) => {
     return { error: "invalid email", success: false };
   }
 
-  const response = (await fetch("https://api.useplunk.com/v1/contacts", {
+  const response = (await fetch("https://api.useplunk.com/v1/track", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${env.PLUNK_API_KEY}`,
     },
-    body: JSON.stringify({ email: email.data, subscribed: true }),
+    body: JSON.stringify({ email: email.data, event: "user-subscribed" }),
   })) as unknown as { success: boolean };
 
   if (response.success) {
