859dff7b0dc0e3e1f0c12134df692667048033cf
Shoubhit Dash <shoubhit2005@gmail.com>
2024-11-18 11:53:48 +0530

    skip lint and typecheck during build, enable ppr and react compiler

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  experimental: {
+    ppr: true,
+    reactCompiler: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
