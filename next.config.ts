946a533983ceaf9b1e1e16150c3059647c4f3336
Shoubhit Dash <shoubhit2005@gmail.com>
2024-11-27 03:33:24 +0530

    switch to uploadthing for images

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,16 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        port: "",
+        pathname: "/a/09ir0bgwfb/**",
+      },
+    ],
+  },
 }
 
 export default nextConfig
