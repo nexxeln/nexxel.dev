77f7c9e61783a25bd73819302eff71083c45ee7b
linustorvaldss <182672946+linustorvaldss@users.noreply.github.com>
2025-06-24 08:43:02 -0500

    refactor: integrate Turnstile verification in newsletter form

--- src/app/blog/newsletter-form.tsx
+++ src/app/blog/newsletter-form.tsx
@@ -4,8 +4,14 @@ import { useFormStatus } from "react-dom";
 import { subscribe } from "../blog/actions";
 import { Turnstile } from "next-turnstile";
 import { env } from "~~/env";
+import { useState } from "react";
 
 export function NewsletterForm() {
+  const [turnstileToken, setTurnstileToken] = useState<string | null>(null);
+
+
+  console.log("zzz", turnstileToken, setTurnstileToken)
+  
   return (
     <form action={subscribe}>
       <div className="flex w-full gap-4">
@@ -18,23 +24,29 @@ export function NewsletterForm() {
             className="w-full border-b border-black bg-inherit p-1 transition-colors placeholder:text-neutral-400 focus:border-neutral-400 focus:outline-none dark:border-white dark:placeholder:text-neutral-600 dark:focus:border-neutral-600"
           />
         </div>
-        <SubmitButton />
+        <SubmitButton disabled={!turnstileToken} />
       </div>
+      
       <div className="mt-4">
-        <Turnstile siteKey={env.NEXT_PUBLIC_CF_TURNSTILE_SITE_KEY} />
+        <Turnstile
+          siteKey={env.NEXT_PUBLIC_CF_TURNSTILE_SITE_KEY}
+          onVerify={setTurnstileToken}
+          onExpire={() => setTurnstileToken(null)}
+        />
+        <input type="hidden" name="cf-turnstile-response" value={turnstileToken ?? ""} />
       </div>
       <div></div>
     </form>
   );
 }
 
-export function SubmitButton() {
+export function SubmitButton({ disabled }: { disabled?: boolean }) {
   const { pending } = useFormStatus();
   return (
     <button
       type="submit"
       className="rounded-md bg-transparent p-1 text-black transition-colors duration-300 ease-in-out hover:bg-black hover:text-white focus:outline-none focus:ring-2 focus:ring-black disabled:cursor-not-allowed disabled:bg-neutral-400 disabled:text-neutral-600 dark:text-white dark:hover:bg-neutral-100 dark:hover:text-black dark:focus:ring-white"
-      disabled={pending}
+      disabled={pending || disabled}
     >
       {pending ? <SvgSpinners3DotsMove /> : "subscribe"}
     </button>
